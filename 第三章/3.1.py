0d655bf8a20c4d93b2de3541fda773f8051135af
Ruijie Lyu <ryanlyu1223@gmail.com>
2026-08-13 04:05:32 +0800

    Delete 第三章 directory

--- 第三章/3.1.py
+++
@@ -1,2 +0,0 @@
-names = ['Leo', 'Jim', 'Danny', 'Ryan']
-print(names[0:4])
--- 第三章/3.3.py
+++
@@ -1,5 +0,0 @@
-trans = ['motor', 'bus', 'taxi']
-message_1 = f'I like to take {trans[0]}'
-message_2 = f'{trans[1].title()} is not so convenient'
-message_3 = f'It is too expensive to take a {trans[2]}'
-print(f'{message_1}\n{message_2}\n{message_3}')
